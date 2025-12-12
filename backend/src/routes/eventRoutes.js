a20562b3109b3c8367fbf0040ed09d8b42ed0d84
Lakhrouf Mohamed <41682397+SUITSHIMED@users.noreply.github.com>
2025-12-12 12:57:13 +0100

    .
    Removed commented-out code for event routes.

--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -1,10 +1,6 @@
-// src/routes/eventRoutes.js
+
 import express from "express";
-// import { getEventInfo } from "../controllers/eventController.js";
 
 const router = express.Router();
-
-// Public Route
-// router.get("/", getEventInfo);
 
 export default router;
