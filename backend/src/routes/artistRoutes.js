38d7fd59c696c0946dbcc827b76db75162c53474
Lakhrouf Mohamed <41682397+SUITSHIMED@users.noreply.github.com>
2025-12-12 12:55:22 +0100

    modefied
    Removed commented-out code for artist routes.

--- backend/src/routes/artistRoutes.js
+++ backend/src/routes/artistRoutes.js
@@ -1,12 +1,6 @@
-// src/routes/artistRoutes.js
+
 import express from "express";
-// You will implement these controllers next
-// import { getAllArtists, getArtistById } from "../controllers/artistController.js";
 
 const router = express.Router();
-
-// Public Routes
-// router.get("/", getAllArtists); 
-// router.get("/:id", getArtistById);
 
 export default router;
